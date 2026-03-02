ea72f17d4e3edc0561b26aa251a39476bc257eb8
fallou-TL <fallou@senyone.sn>
2026-03-02 16:49:32 +0000

    fix MP date peremtion

--- database/migrations/2026_01_15_003431_create_matiere_premieres_table.php
+++ database/migrations/2026_01_15_003431_create_matiere_premieres_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('type');
             $table->string('unite');
             $table->integer('seuil_min');
-            $table->date('peremption_date');
+            $table->date('peremption_date')->nullable();
             $table->timestamps();
         });
     }
